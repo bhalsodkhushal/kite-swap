5ef3eef431abad3b55e57ffd791483df45383f55
Khushal Bhalsod <bhalsodkhushalce@gmail.com>
2025-02-14 17:20:32 +0530

    Change app name
    Change app name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="headerwrap w-full bg-white text-white py-4 px-6 flex flex-wrap justify-between items-center border-gray-200 border-b">
-      <h1 className="logowrap">Swap Token + KiteAI</h1>
+      <h1 className="logowrap">KiteSwap</h1>
 
       <div className="flex items-center space-x-4">
         <span className="text-black">
